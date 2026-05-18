fb1acc630db50469571df094d9f31ecfa60488fc
Leslie Lopez <leslie2515lopez@gmail.com>
2026-05-18 00:05:42 -0600

    Footer component is completed for now

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-100 dark:bg-gray-900">
+    <footer className="flex min-h-screen flex-col bg-gray-100">
       <div className="mx-auto max-w-5xl px-4 py-16 sm:px-6 lg:px-8">
-        <div className="flex justify-center text-teal-600 dark:text-teal-300">
+        <div className="justify-center text-teal-600 dark:text-teal-300">
+          <p>Logo goes here</p>
           <p className="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-500 dark:text-gray-400">
             Copyright © {new Date().getFullYear()}. All rights reserved.
           </p>
