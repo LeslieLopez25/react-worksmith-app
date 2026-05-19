f94610e0f0d5fd46e7c5882e338c1e48079898b7
Leslie Lopez <leslie2515lopez@gmail.com>
2026-05-19 00:36:21 -0600

    Updated the style of the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer.jsx";
-import "./App.css";
 
 export default function App() {
   return (
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,10 @@
 const Footer = () => {
   return (
-    <footer className="flex min-h-screen flex-col bg-gray-100">
-      <div className="mx-auto max-w-5xl px-4 py-16 sm:px-6 lg:px-8">
-        <div className="justify-center text-teal-600 dark:text-teal-300">
-          <p>Logo goes here</p>
-          <p className="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-500 dark:text-gray-400">
-            Copyright © {new Date().getFullYear()}. All rights reserved.
-          </p>
-        </div>
-      </div>
+    <footer className="footer flex items-center justify-center bg-neutral p-4 text-neutral-content sm:footer-horizontal">
+      <aside className="grid-flow-col items-center">
+        {/* {for logo} */}
+        <p>Worksmith Copyright © {new Date().getFullYear()} - All right reserved</p>
+      </aside>
     </footer>
   );
 };
