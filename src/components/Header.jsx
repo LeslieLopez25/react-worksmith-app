9b4b6e3eadaf12165731eab2b18ac3cbcb50cf42
Leslie Lopez <leslie2515lopez@gmail.com>
2026-05-29 01:25:23 -0600

    Updated header css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,40 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className="navbar bg-base-100 shadow-sm">
-      <div className="flex-1">
-        <img src="/assets/logo.png" alt="Worksmith Logo" className="mr-2 h-8 w-8" />
-        <a className="btn text-xl btn-ghost">Worksmith</a>
-      </div>
+      <img src={logo} alt="Worksmith Logo" className="flex h-13" />
+      <a className="btn text-xl btn-ghost">Worksmith</a>
+      <div className="flex-1"></div>
       <div className="flex-none">
-        <ul className="menu menu-horizontal px-4">
-          <li>
-            <a>Write</a>
-          </li>
-          <li>
-            <details>
-              <summary>Name</summary>
-              <ul className="rounded-t-none bg-base-100 p-0">
-                <li>
-                  <a>Portfolio</a>
-                </li>
-                <li>
-                  <a>Logout</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-        </ul>
+        {isLoggedIn ? (
+          <ul className="menu menu-horizontal px-4">
+            <li>
+              <a>Write</a>
+            </li>
+            <li>
+              <details>
+                <summary>Name</summary>
+                <ul className="rounded-t-none bg-base-100 p-0">
+                  <li>
+                    <a>Portfolio</a>
+                  </li>
+                  <li>
+                    <a>Logout</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        ) : (
+          <Link to="/login" className="btn">
+            Login
+          </Link>
+        )}
       </div>
     </div>
   );
