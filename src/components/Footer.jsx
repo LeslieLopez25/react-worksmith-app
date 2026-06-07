a070c22aa3cba5ca6d2371c0d2c23925707f5280
Leslie Lopez <leslie2515lopez@gmail.com>
2026-06-07 01:35:32 -0600

    Updated the footer to include the app's logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
+import logo from "../../assets/logo.png";
+
 const Footer = () => {
   return (
     <footer className="fixed bottom-0 footer flex max-h-screen items-center justify-center bg-neutral p-4 text-neutral-content sm:footer-horizontal">
       <aside className="grid-flow-col items-center">
-        {/* {for logo} */}
+        <img src={logo} alt="Worksmith Logo" className="h-8 w-8" />
         <p>Worksmith Copyright © {new Date().getFullYear()} - All right reserved</p>
       </aside>
     </footer>
