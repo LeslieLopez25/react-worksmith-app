729d4c4cb01e100dad4f26afc334132858f2d769
Leslie Lopez <leslie2515lopez@gmail.com>
2026-06-21 22:52:27 -0600

    Created an indicator component

---
+++ src/components/Indicator.jsx
@@ -0,0 +1,10 @@
+const Indicator = () => {
+  return (
+    <div className="indicator">
+      <span className="indicator-item badge badge-secondary">12</span>
+      <button className="btn">inbox</button>
+    </div>
+  );
+};
+
+export default Indicator;
