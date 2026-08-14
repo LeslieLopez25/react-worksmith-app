fdff384997270fa977dfdd83bc9be21a554ffba4
Leslie Lopez <leslie2515lopez@gmail.com>
2026-08-14 01:27:10 -0600

    Updated hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <img
           src="https://images.pexels.com/photos/12899157/pexels-photo-12899157.jpeg"
-          className="max-w-sm rounded-lg shadow-2xl"
+          className="max-w-xs rounded-lg shadow-2xl"
         />
         <div>
           <h1 className="text-5xl font-bold">Create Your Project</h1>
